d507c4d13c3947d34f6e6549ccd81a2890ce553a
houping wang <whp1473@163.com>
2018-04-11 18:15:30 +0800

    update

---
+++ src/main/java/com/owl/zookeeper/string/StringAppend.java
@@ -0,0 +1,15 @@
+package com.owl.zookeeper.string;
+
+/**
+ * Created by wanghouping on 2018/4/11.
+ *
+ * @author houping wang
+ */
+public class StringAppend {
+
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "2" + a + "3";
+        System.out.println(b);
+    }
+}
---
+++ src/main/java/com/owl/zookeeper/string/StringForAppend.java
@@ -0,0 +1,17 @@
+package com.owl.zookeeper.string;
+
+/**
+ * Created by wanghouping on 2018/4/11.
+ *
+ * @author houping wang
+ */
+public class StringForAppend {
+
+    public static void main(String[] args) {
+        String a = "";
+        for(int i = 0; i < 3; i ++) {
+            a += "1";
+        }
+        System.out.println(a);
+    }
+}
